dcb47e1eeb371ffdbc0fa54ea3d3c1f258c78869
Sam Jackson <samahstan@gmail.com>
2013-03-17 10:25:03 -0500

    Added create_instance, mlnmobject, and class_loc_rot to mlnmone.h

--- mlnmone.h
+++ mlnmone.h
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with MLNM-ONE.  If not, see <http://www.gnu.org/licenses/>.
 
-//Purpose of this file: runscript function
+//Purpose of this file: runscript function, object functions
 
 #include <PHYSFS.h>
 #include <Python.h>
@@ -40,3 +40,41 @@ bool runscript(char pyname[])
 
 }
 
+class class_loc_rot
+{
+
+ float x, y, z;
+
+}
+
+class mlnmobject //This is the class for in game objects
+{
+
+ void init(void)
+ {
+  //This function is run when the object is created
+ }
+
+ /*graphis data*/ draw(/*whatever*/)
+ {
+  //This function carries the data necesary to draw the object
+ }
+
+ class_loc_rot loc, rot; //object's location and rotation
+
+}
+
+mlnmobject create_instance(mlnmobject objecttype, /*attributes*/) //creates an instance of a mlnmobject
+{
+/* objecttype holds the generic object. The attribute parts is variable and expresses the values
+for the different attributes.*/
+
+ mlnmobject *objecttype = new mlnmobject;
+
+ //objecttype.attributes = attributes; //assign all the attributes listed
+
+ *objecttype.init();
+
+ return *objecttype;
+
+}
